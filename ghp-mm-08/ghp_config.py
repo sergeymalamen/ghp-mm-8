d34eaded78ea61e7d564eb6ca9d2f2a24a96c803
sergeymalamen <sergey.malamen@gmail.com>
2025-11-15 20:41:09 +0100

    Last chang

--- ghp-mm-08/ghp_config.py
+++ ghp-mm-08/ghp_config.py
@@ -1,9 +1,21 @@
 #serial port
-SERIAL_PORT = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG009VVA-if00-port0'
+#SERIAL_PORT = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG009VVA-if00-port0'
 
 # MQTT broker settings
-MQTT_BROKER = "localhost"  # Replace with your broker address
-MQTT_PORT = 1883  # Default MQTT port
-MQTT_TOPIC_PREFIX= "GHP"
-MQTT_USERNAME = "celiv"  # Replace with your MQTT username
-MQTT_PASSWORD = "230960"  # Replace with your MQTT password
+#MQTT_BROKER = "localhost"  # Replace with your broker address
+#MQTT_PORT = 1883  # Default MQTT port
+#MQTT_TOPIC_PREFIX= "GHP"
+#MQTT_USERNAME = "celiv"  # Replace with your MQTT username
+#MQTT_PASSWORD = "230960"  # Replace with your MQTT password
+
+
+import json
+
+with open('/data/options.json') as f:
+    opts = json.load(f)
+
+SERIAL_PORT = opts.get("serial_port")
+MQTT_BROKER = opts.get("mqtt_broker")
+MQTT_PORT = opts.get("mqtt_port")
+MQTT_USERNAME = opts.get("mqtt_username")
+MQTT_PASSWORD = opts.get("mqtt_password")
--- ghp-mm-08/run.sh
+++ ghp-mm-08/run.sh
@@ -2,3 +2,20 @@
 echo "🚀 Запуск скрипта"
 . /usr/src/app/venv/bin/activate
 python3 /usr/src/app/ghp-mm2mqtt.py
+
+#Конфигурирование
+
+#CONFIG_PATH=/data/options.json
+
+#MQTT_HOST=$(jq -r '.mqtt_host' $CONFIG_PATH)
+#MQTT_PORT=$(jq -r '.mqtt_port' $CONFIG_PATH)
+#MQTT_USER=$(jq -r '.mqtt_username' $CONFIG_PATH)
+#MQTT_PASS=$(jq -r '.mqtt_password' $CONFIG_PATH)
+#MQTT_PREFIX=$(jq -r '.mqtt_topic_prefix' $CONFIG_PATH)
+
+#exec python3 /ghp-mm2mqtt.py \
+#  --mqtt-host "$MQTT_HOST" \
+#  --mqtt-port "$MQTT_PORT" \
+#  --mqtt-user "$MQTT_USER" \
+#  --mqtt-pass "$MQTT_PASS" \
+#  --mqtt-prefix "$MQTT_PREFIX"
