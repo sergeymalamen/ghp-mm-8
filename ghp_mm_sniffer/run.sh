ef90298fd9007a63b163aa905408a50b6f2d629a
sergeymalamen <sergey.malamen@gmail.com>
2025-11-16 14:09:29 +0100

    Update run.sh

--- ghp_mm_sniffer/run.sh
+++ ghp_mm_sniffer/run.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
-echo "🚀 Запуск скрипта"
-. /usr/src/app/venv/bin/activate
-python3 /usr/src/app/ghp-mm2mqtt.py
+#!/usr/bin/with-contenv bashio
+
+# Чтение параметров из options.json
+SERIAL_PORT=$(bashio::config 'serial_port')
+MQTT_BROKER=$(bashio::config 'mqtt_broker')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USERNAME=$(bashio::config 'mqtt_username')
+MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+
+# Экспортируем переменные в окружение (если нужно)
+export SERIAL_PORT MQTT_BROKER MQTT_PORT MQTT_USERNAME MQTT_PASSWORD
+
+# Запускаем скрипт
+exec python3 /usr/src/ghp-mm2mqtt.py
+
+#echo "🚀 Запуск скрипта"
+#. /usr/src/app/venv/bin/activate
+#python3 /usr/src/app/ghp-mm2mqtt.py
